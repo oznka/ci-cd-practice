d832601117dbaf7ea9a634fce5c18dde2856a999
oznka <ozankarslioglu@gmail.com>
2026-07-17 14:35:02 +0100

    Fixing broken tests

--- test_add.py
+++ test_add.py
@@ -1,10 +1,10 @@
 from add import add
 
 def test_add_positive():
-    assert add(2, 3) == 99
+    assert add(2, 3) == 5
 
 def test_add_negative():
     assert add(-1, -1) == -2
 
 def test_add_incorrect():
-    assert add(2, -1) == 2
+    assert add(2, -1) == 1
